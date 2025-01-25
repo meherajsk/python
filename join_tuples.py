835eab4406a8609e9c75df9dfe5cb2358bfcd876
meheraj <meherajsk579@gmail.com>
2025-01-25 21:17:19 +0530

    done

--- join_tuples.py
+++ join_tuples.py
@@ -2,3 +2,13 @@ tuple1 = ("a", "b", "c", "d")
 tuple2 = (1, 2, 3, 4)
 tuple3 = tuple1 + tuple2
 print(tuple3) # ('a', 'b', 'c', 'd', 1, 2, 3, 4)
+
+tuple4 = tuple1 * 3
+print(tuple4)
+
+x = tuple4.count("a")
+print(x)
+y = tuple4.index("b")
+print(y)
+z = tuple4.index("b", 2)
+print(z)
