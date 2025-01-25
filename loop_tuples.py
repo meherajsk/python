397d5103a3668d7c3b1ced9a18b5cb27e22aa34f
meheraj <meherajsk579@gmail.com>
2025-01-25 20:11:36 +0530

    done

---
+++ loop_tuples.py
@@ -0,0 +1,14 @@
+tuples = ("apple", "banana", "coco", "mango")
+for x in tuples:
+    print(x)
+
+print()
+for x in range(len(tuples)):
+    print(tuples[x])
+    
+
+print()
+x = 0
+while x < len(tuples):
+    print(tuples[x])
+    x +=1
