61a7382ced0212b67b789022b2118078ebc0c2be
meheraj <meherajsk579@gmail.com>
2025-01-25 19:15:03 +0530

    done

--- tuples.py
+++ tuples.py
@@ -2,3 +2,18 @@
 
 mytuple = ("apple", "banana", "cherry")
 print(mytuple)
+
+print(len(mytuple))
+
+#one item tuple remember the comma
+thistuple = ("apple",)
+print(type(thistuple))
+
+#it can be any data type and contain different data type
+#tuple are defined as object with data type tuple
+
+
+#making tuple using tuple() contructor
+thistuple1 = tuple(("apple", "mango", "banana", "orange"))
+print(thistuple1)
+
