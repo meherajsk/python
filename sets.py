61b9162b1dac2ddb37bb276cae47313ceba5c9cb
meheraj <meherajsk579@gmail.com>
2025-01-25 21:25:20 +0530

    done

---
+++ sets.py
@@ -0,0 +1,10 @@
+#store multiple items in a single variable
+#unordered and unindexed. No duplicate members. unchangeable but can remove and add items
+
+thisset = {"apple", "banana", "cherry"}
+print(thisset) # {'banana', 'apple', 'cherry'} cause unordered
+
+#cant cahnge items but can add items or remove items
+
+thisset = {"apple", "banana", "cherry", "apple"}    
+print(thisset) # {'banana', 'apple', 'cherry'} cause no duplicate members  
