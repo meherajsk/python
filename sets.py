73f334a09d40f2b41b5226c368f156220579fe47
meheraj <meherajsk579@gmail.com>
2025-01-25 21:35:52 +0530

    done

--- sets.py
+++ sets.py
@@ -8,3 +8,16 @@ print(thisset) # {'banana', 'apple', 'cherry'} cause unordered
 
 thisset = {"apple", "banana", "cherry", "apple"}    
 print(thisset) # {'banana', 'apple', 'cherry'} cause no duplicate members  
+
+thisset1 = {"apple", "banana", "cherry", 1, 2, True, False, 0}
+print(thisset1) # True and 1 and 0 and False are considered as same 
+
+
+print(len(thisset1)) 
+
+#can be any data typea and can be mixed data types
+#set is an object with data type set
+#set() constructor to make a set
+
+myset = set(("apple", "banana", "cherry"))
+print(myset) # {'banana', 'apple', 'cherry'} cause unordered
