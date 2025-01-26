a07de44e4c6886774c2ee5422106918cb7040a19
meheraj <meherajsk579@gmail.com>
2025-01-26 15:32:20 +0530

    done

--- access_set.py
+++ access_set.py
@@ -1,7 +1,16 @@
-thisset = {"apple", "banana", "cherry"}
+thisset = {"apple", "bananah", "cherry"}
 
 for x in thisset:
     print(x)
 
 print("banana" in thisset)
 print("orange" not in thisset)
+
+thisset.add("orange")
+print(thisset)
+
+
+thisset2 = {"meheraj", "amirul", "mominur"}
+thisset.update(thisset2)
+print(thisset)
+
