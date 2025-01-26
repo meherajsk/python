98f4b0209ef1c4509726b2e1f0d11779dc40b0f8
meheraj <meherajsk579@gmail.com>
2025-01-26 15:50:15 +0530

    done

--- remove_set_item.py
+++ remove_set_item.py
@@ -1,3 +1,25 @@
 thisset = {"apple", "banana", "cherry"}
 thisset.remove("banana")
 print(thisset)
+
+#thisset.remove("kiwi") #item does not exist then it will show error
+#print(thisset)
+
+thisset.add("banana")
+print(thisset)
+
+thisset.discard("banana")
+print(thisset)
+#if item doesnot exists then it will not show error
+
+x = thisset.pop()#return value is removed item and it removes item randomly
+print(x)
+print(thisset)
+
+thisset.clear()#empties set
+print(thisset)
+
+thisset = {"apple", "banana","cherry"}
+del thisset
+print(thisset)
+
