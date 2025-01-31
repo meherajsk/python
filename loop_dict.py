7dcf3b24bcae79236320bfdd97300536a033d30c
meheraj <meherajsk579@gmail.com>
2025-01-31 19:26:20 +0530

    done

--- loop_dict.py
+++ loop_dict.py
@@ -1,4 +1,17 @@
 from change_items_dictionary import thisdict
 for x in thisdict:
     print(x)
-    
+
+print()
+# printing value
+for x in thisdict:
+    print(thisdict[x])
+
+print()
+for x in thisdict.values():
+    print(x)
+
+print()
+# loop through key and value
+for x, y in thisdict.items():
+    print(x,"   ",y)
