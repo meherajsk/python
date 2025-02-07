634971333fa6050c9151c90f8ba63d1b4ec6f8f7
meheraj <meherajsk579@gmail.com>
2025-02-07 22:14:12 +0530

    done

---
+++ while_loops.py
@@ -0,0 +1,28 @@
+i = 1
+while 1 < 6:
+    print(i)
+    if i ==4:
+        break
+    i += 1
+
+print()
+
+# continue stops the current iteration and continue with next
+i = 0
+while i < 5:
+    i += 1
+    if i == 3:
+        continue
+    print(i)
+
+
+
+
+print()
+i =1 
+while i < 7:
+    print(i)
+    i += 1
+else:
+    print("i is nolonger less than 7")
+
