da250928a2d141bda0ae80b862a0f9ca5877bedb
meheraj <meherajsk579@gmail.com>
2025-02-08 21:38:48 +0530

    done

---
+++ for_loops.py
@@ -0,0 +1,75 @@
+fruit = ["apple", "banana", "cherry", "kiwi"]
+for x in fruit:
+    print(x)
+
+print()
+for x in "banana":
+    print(x)
+
+print()
+for x in fruit:
+    print(x)
+    if x == "banana":
+        break
+
+#exit the loop when x is banana
+print()
+for x in fruit:
+    if x == "banana":
+        break
+    print(x)
+
+
+print()
+for x in fruit:
+    if x == "banana":
+        continue
+    print(x)
+
+#range()
+print()
+for x in range(4):
+    print(x)
+
+
+print()
+for x in range(2, 4):
+    print(x)
+
+
+print()
+for x in range(2, 30, 3):
+    print(x)
+
+
+print()
+print("a block of code to be executed when the loop is finished")
+for x in range(9):
+    print(x)
+else:
+    print("finished")
+
+
+
+print()
+print("The else block will NOT be executed if the loop is stopped by a break statement")
+for x in range(9):
+    if x == 7: break
+        #pass
+    print(x)
+else:
+    print("finished")
+
+
+print()
+print("Print each adjective for every fruit:")
+adj = ["red", "healthy", "big", "tasty"]
+for x in adj:
+    for y in fruit:
+        print(x, y)
+
+    print()
+        
+
+
+
