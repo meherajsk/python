cb85e2c94ccde8e1c9e1ff28131c99b00bc134f5
meheraj <meherajsk579@gmail.com>
2025-02-13 19:13:14 +0530

    done

---
+++ lamda.py
@@ -0,0 +1,11 @@
+"""
+A lambda function is a small anonymous function.
+A lambda function can take any number of arguments, but can only have one expression.
+"""
+
+#lambda arguments : expression
+x = lambda a : a + 10
+print(x(5))
+
+x = lambda m, n : m + n
+print(x("meheraj", "sk"))
