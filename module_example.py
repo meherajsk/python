c0ec1d2988062b86803b1a89bde17a5efce8815b
meheraj <meherajsk579@gmail.com>
2025-02-21 00:38:25 +0530

    done

--- module_example.py
+++ module_example.py
@@ -1,8 +1,14 @@
 #from m_module import *
 import m_module as m
+import platform
 
 m.greeting("Jonathan")
 
 x = m.person1["age"]
+print(x)
+
+y = dir(m)
+print(y)
+x = platform.system()
 print(x)
 
