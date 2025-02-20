07f648193d68d9be2ea89bc370b604e72de00244
meheraj <meherajsk579@gmail.com>
2025-02-21 00:12:04 +0530

    file added

---
+++ m_module.py
@@ -0,0 +1,11 @@
+"""module is a code library"""
+
+"""def greeting(name):
+    print("hello, " +name)
+"""
+#variables
+person1 = {
+    "name": "John",
+    "age": 36,
+    "country": "Norway" 
+}
---
+++ module_example.py
@@ -0,0 +1,7 @@
+import m_module
+
+m_module.greeting("Jonathan")
+
+#x = m_module.person1["age"]
+#print(x)
+
