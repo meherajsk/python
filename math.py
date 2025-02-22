9c615b48a8172e7102275d8c1180cbfcdf7cc5b5
meheraj <meherajsk579@gmail.com>
2025-02-22 10:44:24 +0530

    file added

---
+++ math.py
@@ -0,0 +1,31 @@
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+
+x = abs(-7.25)
+print(x)
+
+
+x = pow(4, 3)
+print(x)
+
+"""to use more math functions, you need to import the math module:"""
+import math
+print()
+x = math.sqrt(64)
+print(x)
+
+"""method to round a number to the nearest integer:"""
+
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x) # returns 2
+print(y) # returns 1
+
+
+x = math.pi
+print(x)
