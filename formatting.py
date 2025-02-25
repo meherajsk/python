1e156a033b8b900f0bf3ff5f7c03ce6cf8706dfc
meheraj <meherajsk579@gmail.com>
2025-02-25 12:08:48 +0530

    done

--- formatting.py
+++ formatting.py
@@ -101,3 +101,25 @@ itemno = 567
 price = 49
 myorder = "I want {} pieces of item number {} for {:.2f} dollars."
 print(myorder.format(quantity, itemno, price))
+
+"""You can use index numbers (a number inside the curly brackets {0}) 
+to be sure the values are placed in the correct placeholders:"""
+
+print()
+quantity = 3
+itemno = 567
+price = 49
+myorder = "I want {0} pieces of item number {1} for {2:.2f} dollars."
+print(myorder.format(quantity, itemno, price))
+
+
+print()
+age = 36
+name = "John"
+txt = "His name is {1}. {1} is {0} years old."
+print(txt.format(age, name))
+
+
+print()
+myorder = "I have a {carname}, it is a {model}."
+print(myorder.format(carname = "Ford", model = "Mustang"))
