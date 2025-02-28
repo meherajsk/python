2f05539cb0fc94a353e95c73b26171e649e3ca43
meheraj <meherajsk579@gmail.com>
2025-03-01 00:31:44 +0530

    done

--- matplotlib_start.py
+++ matplotlib_start.py
@@ -1,23 +1,23 @@
 
 
 # Most of the Matplotlib utilities lies under the pyplot submodule, 
-# and are usually imported under the plt alias:
+# and are usually imported under theplt alias:
 
 import matplotlib.pyplot as plt
 import numpy as np
+'''
+x = np.array([0, 6, 7, 14, 20])
+y = np.array([0, 250, 23, 340, 60])
 
-#x = np.array([0, 6, 7, 14, 20])
-#y = np.array([0, 250, 23, 340, 60])
-
-#plt.plot(x, y) #prints line
-#plt.plot(x, y, "o") #prints only points not line
-#plt.show()
+plt.plot(x, y) #prints line
+plt.plot(x, y, "o") #prints only points not line
+plt.show()
 
 
 # marker : you can use the keyword to emphasize each point with aspecified marker
 z = np.array([3, 8, 1, 10])
 plt.plot(z, marker = '*')
-#plt.plot(z, marker = 'o')
+plt.plot(z, marker = 'o')
 plt.show()
 
 """
@@ -52,7 +52,6 @@ Marker	Description
 
 
 ypoints = np.array([3, 8, 1, 10])
-
 plt.plot(ypoints, 'o-.r')
 plt.show()
 
@@ -80,7 +79,6 @@ Color Syntax	Description
 #You can use the keyword argument markersize or the shorter version, 
 # ms to set the size of the markers:
 ypoints = np.array([3, 8, 1, 10])
-
 plt.plot(ypoints, marker = 'o', ms = 15)
 plt.show()
 
@@ -89,7 +87,6 @@ plt.show()
 # shorter mec to set the color of the edge of the markers:
 
 ypoints = np.array([3, 8, 1, 10])
-
 plt.plot(ypoints, marker = 'o', ms = 20, mec = 'r')
 plt.show()
 
@@ -98,7 +95,6 @@ plt.show()
 # shorter mfc to set the color inside the edge of the markers:
 
 ypoints = np.array([3, 8, 1, 10])
-
 plt.plot(ypoints, marker = 'o', ms = 20, mfc = 'r')
 plt.show()
 
@@ -107,13 +103,38 @@ plt.show()
 
 """both"""
 ypoints = np.array([3, 8, 1, 10])
-
 plt.plot(ypoints, marker = 'o', ms = 20, mec = 'r', mfc = 'r')
 plt.show()
 
 
 
-
-
+...
+plt.plot(ypoints, marker = 'o', ms = 20, mec = 'hotpink', mfc = 'hotpink')
+...
 plt.plot(ypoints, marker = 'o', ms = 20, mec = '#4CAF50', mfc = '#4CAF50')
+plt.show()
+
+
+
+y1 = np.array([3, 8, 1, 10])
+y2 = np.array([6, 2, 7, 11])
+plt.plot(y1)
+plt.plot(y2)
+
+plt.show()
+
+'''
+
+
+"""label"""
+
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+plt.plot(x, y)
+
+font1 = {'family':'serif','color':'blue','size':20}
+font2 = {'family':'serif','color':'darkred','size':15}
+plt.title("Sports Watch Data", loc ='left', fontdict = font1) #important
+plt.xlabel("Average Pulse", fontdict = font2 ) #important
+plt.ylabel("Calorie Burnage", fontdict =font2) #important
 plt.show()
